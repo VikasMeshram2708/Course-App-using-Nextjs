b746814c5439317e7a9c6d4fa2eee9d238365aac
VikasMeshram2708 <meshramvikas744@gmail.com>
2025-05-11 16:57:49 +0530

    :tada: check: checking the base url

--- components/courses/share-btn.tsx
+++ components/courses/share-btn.tsx
@@ -6,6 +6,7 @@ import { Share2 } from "lucide-react";
 export default function ShareBtn({ courseId }: { courseId: string }) {
   function handleShare() {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
+    console.log("baseUrl", baseUrl);
     const courseUrl = `${baseUrl}/courses/${courseId}`;
     window.navigator.clipboard.writeText(courseUrl);
     alert("Link Copied!");
